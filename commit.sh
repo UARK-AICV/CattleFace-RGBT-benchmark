65c31ca9dfab5a18d5f13d3f24827c8dd845c090
ecoffman1 <ethanjackc@gmail.com>
2024-06-12 00:43:12 -0500

    Update

--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git add .
-git commit -m "update"
-git push origin main
+git remote add origin https://github.com/UARK-AICV/CattleFace-RGBT-benchmark.git
+git branch -M main
+git push -u origin main
